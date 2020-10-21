9f8990f5c8ce303114b3217a16f7447afffdb3c0
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-10-22 03:06:20 +0900

    bug fix

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ rm -rf ${PREDICTION_ROOT} ${ENSEMBLED_PREDICTION_ROOT} ${REFINED_PREDICTION_ROOT
 /work/scripts/test/test_cnns.sh ${TEST_ROOT}
 
 # postprocess predictions
-/work/scripts/test/postprocess.sh ${TEST_DIR} ${OUTPUT_CSV_PATH}
+/work/scripts/test/postprocess.sh ${TEST_ROOT} ${OUTPUT_CSV_PATH}
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo 'Total time for testing: ' $(($ELAPSED_TIME / 60 + 1)) '[min]'
