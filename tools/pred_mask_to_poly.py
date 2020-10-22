5db9c43f8fdf1c26034f4fe8adf7ad6c0f25acc1
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-10-22 19:09:53 +0900

    update

--- tools/pred_mask_to_poly.py
+++ tools/pred_mask_to_poly.py
@@ -14,6 +14,73 @@ from spacenet7_model.utils import (
     map_wrapper)
 from tqdm import tqdm
 
+
+def generate_polys(mask_path, output_path, config):
+    """[summary]
+
+    Args:
+        mask_path ([type]): [description]
+        output_path ([type]): [description]
+        config ([type]): [description]
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        [type]: [description]
+    """
+    pred_array = load_prediction_from_png(mask_path,
+                                          n_channels=len(config.INPUT.CLASSES))
+
+    footprint_channel = config.INPUT.CLASSES.index('building_footprint')
+    boundary_channel = config.INPUT.CLASSES.index('building_boundary')
+    contact_channel = config.INPUT.CLASSES.index('building_contact')
+
+    footprint_score = pred_array[footprint_channel]
+    boundary_score = pred_array[boundary_channel]
+    contact_score = pred_array[contact_channel]
+    building_score = compute_building_score(
+        footprint_score,
+        boundary_score,
+        contact_score,
+        alpha=config.BOUNDARY_SUBTRACT_COEFF,
+        beta=config.CONTACT_SUBTRACT_COEFF)
+
+    if config.METHOD_TO_MAKE_POLYGONS == 'contours':
+        polys = gen_building_polys_using_contours(
+            building_score,
+            config.BUILDING_MIM_AREA_PIXEL,
+            config.BUILDING_SCORE_THRESH,
+            simplify=False,
+            output_path=output_path)
+    elif config.METHOD_TO_MAKE_POLYGONS == 'watershed':
+        polys = gen_building_polys_using_watershed(
+            building_score,
+            config.WATERSHED_SEED_MIN_AREA_PIXEL,
+            config.WATERSHED_MIN_AREA_PIXEL,
+            config.WATERSHED_SEED_THRESH,
+            config.WATERSHED_MAIN_THRESH,
+            output_path=output_path)
+    elif config.METHOD_TO_MAKE_POLYGONS == 'watershed2':
+        polys = gen_building_polys_using_watershed_2(
+            footprint_score,
+            boundary_score,
+            contact_score,
+            config.WATERSHED2_SEED_MIN_AREA_PIXEL,
+            config.WATERSHED2_MIN_AREA_PIXEL,
+            config.WATERSHED2_SEED_THRESH,
+            config.WATERSHED2_MAIN_THRESH,
+            config.WATERSHED2_SEED_BOUNDARY_SUBTRACT_COEFF,
+            config.WATERSHED2_SEED_CONTACT_SUBTRACT_COEFF,
+            config.WATERSHED2_BOUNDARY_SUBTRACT_COEFF,
+            config.WATERSHED2_CONTACT_SUBTRACT_COEFF,
+            output_path=output_path)
+    else:
+        raise ValueError()
+
+    return polys
+
+
 if __name__ == '__main__':
     t0 = timeit.default_timer()
 
@@ -34,10 +101,7 @@ if __name__ == '__main__':
 
     print('preparing input args...')
     input_args = []
-
     for i, aoi in enumerate(aois):
-        print(f'processing {aoi} ({i + 1}/{len(aois)}) ...')
-
         paths = glob(os.path.join(input_root, aoi, '*.png'))
         paths.sort()
 
@@ -45,59 +109,11 @@ if __name__ == '__main__':
         os.makedirs(out_dir, exist_ok=False)
 
         for path in tqdm(paths):
-            pred_array = load_prediction_from_png(path,
-                                                  n_channels=len(
-                                                      config.INPUT.CLASSES))
-
-            footprint_channel = config.INPUT.CLASSES.index(
-                'building_footprint')
-            boundary_channel = config.INPUT.CLASSES.index('building_boundary')
-            contact_channel = config.INPUT.CLASSES.index('building_contact')
-
-            footprint_score = pred_array[footprint_channel]
-            boundary_score = pred_array[boundary_channel]
-            contact_score = pred_array[contact_channel]
-            building_score = compute_building_score(
-                footprint_score,
-                boundary_score,
-                contact_score,
-                alpha=config.BOUNDARY_SUBTRACT_COEFF,
-                beta=config.CONTACT_SUBTRACT_COEFF)
-
             filename = os.path.basename(path)
             filename, _ = os.path.splitext(filename)
             output_path = os.path.join(out_dir, f'{filename}.geojson')
 
-            if config.METHOD_TO_MAKE_POLYGONS == 'contours':
-                simplify = False
-                input_args.append([
-                    gen_building_polys_using_contours,
-                    config.BUILDING_MIM_AREA_PIXEL,
-                    config.BUILDING_SCORE_THRESH, simplify, output_path
-                ])
-            elif config.METHOD_TO_MAKE_POLYGONS == 'watershed':
-                input_args.append([
-                    gen_building_polys_using_watershed, building_score,
-                    config.WATERSHED_SEED_MIN_AREA_PIXEL,
-                    config.WATERSHED_MIN_AREA_PIXEL,
-                    config.WATERSHED_SEED_THRESH, config.WATERSHED_MAIN_THRESH,
-                    output_path
-                ])
-            elif config.METHOD_TO_MAKE_POLYGONS == 'watershed2':
-                input_args.append([
-                    gen_building_polys_using_watershed_2, footprint_score,
-                    boundary_score, contact_score,
-                    config.WATERSHED2_SEED_MIN_AREA_PIXEL,
-                    config.WATERSHED2_MIN_AREA_PIXEL,
-                    config.WATERSHED2_SEED_THRESH,
-                    config.WATERSHED2_MAIN_THRESH,
-                    config.WATERSHED2_SEED_BOUNDARY_SUBTRACT_COEFF,
-                    config.WATERSHED2_SEED_CONTACT_SUBTRACT_COEFF,
-                    config.WATERSHED2_BOUNDARY_SUBTRACT_COEFF,
-                    config.WATERSHED2_CONTACT_SUBTRACT_COEFF, output_path
-                ])
-            else:
-                raise ValueError()
+            input_args.append([generate_polys, path, output_path])
 
     print('running multiprocessing...')
     pool = mp.Pool(processes=n_thread)
