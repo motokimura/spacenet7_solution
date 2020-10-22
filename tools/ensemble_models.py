0ddf8231b4d4341ba2c983dad5df08fb782b6b63
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-10-22 20:21:41 +0900

    update

--- tools/ensemble_models.py
+++ tools/ensemble_models.py
@@ -2,6 +2,7 @@
 
 import json
 import os.path
+import multiprocessing as mp
 import timeit
 
 import numpy as np
@@ -12,8 +13,43 @@ from spacenet7_model.configs import load_config
 from spacenet7_model.utils import (dump_prediction_to_png, ensemble_subdir,
                                    experiment_subdir, get_aoi_from_path,
                                    get_image_paths, load_prediction_from_png,
-                                   val_list_filename)
+                                   map_wrapper, val_list_filename)
 from tqdm import tqdm
+
+
+def ensemble_preds(image_path, aoi, out_dir, weights, config):
+    """[summary]
+
+    Args:
+        image_path ([type]): [description]
+        aoi ([type]): [description]
+        out_root ([type]): [description]
+        weights ([type]): [description]
+        config ([type]): [description]
+    """
+    image_orig = io.imread(image_path)
+    roi_mask = image_orig[:, :, 3] > 0
+    h, w = roi_mask.shape
+    ensembled_score = np.zeros(shape=[len(config.INPUT.CLASSES), h, w])
+
+    image_filename = os.path.basename(image_path)
+    array_filename, _ = os.path.splitext(image_filename)
+    array_filename = f'{array_filename}.png'
+
+    for exp_id, weight in zip(config.ENSEMBLE_EXP_IDS, weights):
+        exp_subdir = experiment_subdir(exp_id)
+        score_array = load_prediction_from_png(
+            os.path.join(config.PREDICTION_ROOT, exp_subdir, aoi,
+                         array_filename),
+            n_channels=len(config.INPUT.CLASSES))
+        score_array[:, np.logical_not(roi_mask)] = 0
+        assert score_array.min() >= 0 and score_array.max() <= 1
+        ensembled_score += score_array * weight
+
+    assert ensembled_score.min() >= 0 and ensembled_score.max() <= 1
+    dump_prediction_to_png(os.path.join(out_dir, array_filename),
+                           ensembled_score)
+
 
 if __name__ == '__main__':
     t0 = timeit.default_timer()
@@ -22,6 +58,10 @@ if __name__ == '__main__':
 
     assert len(config.ENSEMBLE_EXP_IDS) >= 1
     N = len(config.ENSEMBLE_EXP_IDS)
+
+    n_thread = config.ENSEMBLE_NUM_THREADS
+    n_thread = n_thread if n_thread > 0 else mp.cpu_count()
+    print(f'N_thread for multiprocessing: {n_thread}')
 
     # prepare ensemble weights
     if len(config.ENSEMBLE_WEIGHTS) == 0:
@@ -48,36 +88,21 @@ if __name__ == '__main__':
     out_root = os.path.join(config.ENSEMBLED_PREDICTION_ROOT, subdir)
     os.makedirs(out_root, exist_ok=False)
 
+    print('preparing input args...')
+    input_args = []
     for image_path in tqdm(image_paths):
-        image_orig = io.imread(image_path)
-        image = image_orig[:, :, :3]
-        roi_mask = image_orig[:, :, 3] > 0
-
-        h, w = roi_mask.shape
-        ensembled_score = np.zeros(shape=[len(config.INPUT.CLASSES), h, w])
-
         aoi = get_aoi_from_path(image_path)
-
-        image_filename = os.path.basename(image_path)
-        array_filename, _ = os.path.splitext(image_filename)
-        array_filename = f'{array_filename}.png'
-
+        # prepare aoi sub directory to output ensemble results
         out_dir = os.path.join(out_root, aoi)
         os.makedirs(out_dir, exist_ok=True)
+        input_args.append(
+            [ensemble_preds, image_path, aoi, out_dir, weights, config])
 
-        for exp_id, weight in zip(config.ENSEMBLE_EXP_IDS, weights):
-            exp_subdir = experiment_subdir(exp_id)
-            score_array = load_prediction_from_png(
-                os.path.join(config.PREDICTION_ROOT, exp_subdir, aoi,
-                             array_filename),
-                n_channels=len(config.INPUT.CLASSES))
-            score_array[:, np.logical_not(roi_mask)] = 0
-            assert score_array.min() >= 0 and score_array.max() <= 1
-            ensembled_score += score_array * weight
-
-        assert ensembled_score.min() >= 0 and ensembled_score.max() <= 1
-        dump_prediction_to_png(os.path.join(out_dir, array_filename),
-                               ensembled_score)
+    print('running multiprocessing...')
+    pool = mp.Pool(processes=n_thread)
+    with tqdm(total=len(input_args)) as t:
+        for _ in pool.imap_unordered(map_wrapper, input_args):
+            t.update(1)
 
     elapsed = timeit.default_timer() - t0
     print('Time: {:.3f} min'.format(elapsed / 60.0))
